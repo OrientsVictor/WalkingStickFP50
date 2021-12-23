8e2f9bc126013ef1ea51890d6a632ca0409b04c1
SourjyaSarkar2005 <sarkarsourjya@outlook.com>
2021-12-23 14:21:05 +0530

    added speech recognition in some extent... version 0.1.1

--- app/app.py
+++ app/app.py
@@ -1,7 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import speech_recognition as sr
+
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    transcript = ""
+    if request.method == "POST": 
+        print("form received")
+
+        if "file" not in request.files:
+            return redirect(request.url)
+            
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+
+        if file:
+            recognizer = sr.Recognizer()
+            audioFile = sr.AudioFile(file)
+            with audioFile as source:
+                data = recognizer.record(source)
+            transcript = recognizer.recognize_google(data, key=None)
+
+    return render_template("index.html", transcript=transcript)
